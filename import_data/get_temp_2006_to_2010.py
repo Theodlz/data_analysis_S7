6739fd7b684985d68a14b802166064825167cbb3
Theodlz <theophile.dulaz@gmail.com>
2021-11-24 18:25:31 +0000

    Changes to be committed: 	modified:   import_data/get_temp_2006_to_2010.py

--- import_data/get_temp_2006_to_2010.py
+++ import_data/get_temp_2006_to_2010.py
@@ -8,35 +8,38 @@ import csv
 
 import datetime as dt
 
-years = [2006,2007,2008,2009,2010]
-
-for year in years:
-    date = []
-    temp_max = []
-    temp_min = []
-    soup = BeautifulSoup(requests.get("https://www.infoclimat.fr/climatologie/normales-records/{}-{}/paris-montsouris/details/07156.html".format(year,year)).content, "html.parser")
-    script = soup.find("script", text=re.compile("Highcharts.Chart")).text
-    parsed = js2xml.parse(script)
-    for parsed2 in parsed.xpath("//property[@name='series']/array/object"):
-        type = parsed2.xpath(".//property[@name='name']/string/text()")
-        if type[0] in ['Température maximale moyenne','Température minimale moyenne']:
-            print(type[0])
-            for d in parsed2.xpath(".//property[@name='data']"):
-                if len(date) == 0:
-                    timestamp = d.xpath(".//array/object/property[@name='x']/number/@value")
-                    for indx, ts in enumerate(timestamp):
-                        timestamp[indx] = dt.datetime.fromtimestamp(int(ts[:10])).date()
-                    date = timestamp
-                temperature = d.xpath(".//array/object/property[@name='y']/number/@value")
-                if type[0] == 'Température maximale moyenne' and len(temp_max)==0:
-                    temp_max = temperature
-                if type[0] == 'Température minimale moyenne' and len(temp_min)==0:
-                    temp_min = temperature
-
-
+def generate_temp_data():
+    years = [2006,2007,2008,2009,2010]
+    for year in years:
+        print('downloading data of {} ...'.format(year))
+        date = []
+        temp_max = []
+        temp_min = []
+        soup = BeautifulSoup(requests.get("https://www.infoclimat.fr/climatologie/normales-records/{}-{}/paris-montsouris/details/07156.html".format(year,year)).content, "html.parser")
+        script = soup.find("script", text=re.compile("Highcharts.Chart")).text
+        parsed = js2xml.parse(script)
+        for parsed2 in parsed.xpath("//property[@name='series']/array/object"):
+            type = parsed2.xpath(".//property[@name='name']/string/text()")
+            if type[0] in ['Température maximale moyenne','Température minimale moyenne']:
+                for d in parsed2.xpath(".//property[@name='data']"):
+                    if len(date) == 0:
+                        timestamp = d.xpath(".//array/object/property[@name='x']/number/@value")
+                        for indx, ts in enumerate(timestamp):
+                            timestamp[indx] = dt.datetime.fromtimestamp(int(ts[:10])).date()
+                        date = timestamp
+                    temperature = d.xpath(".//array/object/property[@name='y']/number/@value")
+                    if type[0] == 'Température maximale moyenne' and len(temp_max)==0:
+                        temp_max = temperature
+                    if type[0] == 'Température minimale moyenne' and len(temp_min)==0:
+                        temp_min = temperature
 
 
-    with open("temperatures_{}.csv".format(year), "w") as f:
-        writer = csv.writer(f)
-        writer.writerow(["Date", "min_t", "max_t"])
-        writer.writerows(zip(date,temp_min,temp_max))
+
+
+        with open("temperatures_{}.csv".format(year), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow(["Date", "min_t", "max_t"])
+            writer.writerows(zip(date,temp_min,temp_max))
+        print('download complete ! \n')
+
+generate_temp_data()
