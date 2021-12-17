0c519db8060a3ded65e825b551c84154c51328df
InternetXplorer <73933562+InternetXplorer@users.noreply.github.com>
2021-12-17 01:43:00 +0100

    updated readme and code comments

--- Flask/README.md
+++ Flask/README.md
@@ -1,5 +1,5 @@
-Before Running app.py :
+Before Running app.py to use the model's web interface:
 	In your terminal, go to /data_analysis_S7/ and run "pip install -r requirements.txt"
-	Execute all cells in the Jupyter Notebook : /data_analysis_S7/RANDOM_FOREST_REGRESSION.ipynb
-	Now you're good to go ! Just run in your terminal "python app.py" or "python3 app.py", then you can access the web page in your browser at the following 	address : http://127.0.0.1:5000/
+	Execute all cells in the Jupyter Notebook : /data_analysis_S7/RANDOM_FOREST_REGRESSION.ipynb (to train the models, can take several minutes)
+	Now you're good to go ! Just run in your terminal "python app.py" or "python3 app.py", then you can access the web page in your browser at the following address : http://127.0.0.1:5000/
 	You can find a demo version with pre selected values at http://127.0.0.1:5000/demo
--- Flask/app.py
+++ Flask/app.py
@@ -14,27 +14,8 @@ def home():
 def demo():
     return render_template('index_demo.html')
 
-@app.route('/predict_all_variables',methods=['POST'])
-def predict_all_variables():
-    #retrieve input
-    inputs = [str(x) for x in request.form.values()]
-    output = predict(inputs)
-    
-    # output = scaled_inputs
-    return render_template('index.html', prediction_text=output)
 
-
-@app.route('/demo_result',methods=['POST'])
-def demo_predict():
-    #retrieve input
-    inputs = [str(x) for x in request.form.values()]
-    output = predict(inputs)
-    
-    # output = scaled_inputs
-    return render_template('index_demo.html', prediction_text=output)
-
-
-def predict(inputs):
+def predict(inputs): #takes the html page's inputs, processes and formats them, then uses the ML model loaded from disk to predict the power consumptionof the next time step from these inputs.
     timeframe = inputs[0]
     timeframe = timeframe.lower()
     date_time = datetime.datetime.fromisoformat(inputs[-1])
@@ -70,45 +51,31 @@ def predict(inputs):
     prediction = pred_row_unscaled[0,0]
 
     prediction = round(prediction, 3)
-    output = f'Avg global active power in the next {timeframe} from {str(date_time)} should be {prediction} kiloWatt.'
+    output = f'Average global active power in the next {timeframe} from {str(date_time)} should be {prediction} kiloWatt.'
 
     return output
 
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     str_features = [str(x) for x in request.form.values()]
-#     timeframe = str_features[0]
-#     timeframe = timeframe.lower()
-#     date_time = datetime.datetime.fromisoformat(str_features[-1])
-#     str_features.pop(0)
-#     str_features.pop(-1)
-#     str_features.append(date_time.hour + date_time.minute/60)
-#     str_features.append(date_time.day)
-#     str_features.append(date_time.month)
-#     final_features = [np.array(str_features)]
-
-#     model_type = "RandomForestRegressor"
-#     variable_to_predict = "Global_active_power"
-
-#     model = pickle.load(open(f'../models/{timeframe}/{model_type}/{variable_to_predict}.pkl', 'rb'))
-#     print("type of model : ", type(model))
-
-#     prediction = model.predict(final_features)
-#     output = round(prediction[0], 3)
+@app.route('/predict_all_variables',methods=['POST'])
+def predict_all_variables():
+    #retrieve input
+    inputs = [str(x) for x in request.form.values()]
+    output = predict(inputs)
     
-#     return render_template('index.html', prediction_text=f'Avg global active power in the next {timeframe} from {str(date_time)} should be {output} kiloWatt.')
+    return render_template('index.html', prediction_text=output)
 
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-#     '''
-#     For direct API calls trought request
-#     '''
-#     data = request.get_json(force=True)
-#     prediction = model.predict([np.array(list(data.values()))])
+# I would have liked having more time to create a page dedicated to multistep prediction, plotting the results on a graph.
 
-#     output = prediction[0]
-#     return jsonify(output)
+
+@app.route('/demo_result',methods=['POST'])
+def demo_predict():
+    #retrieve input
+    inputs = [str(x) for x in request.form.values()]
+    output = predict(inputs)
+    
+    # output = scaled_inputs
+    return render_template('index_demo.html', prediction_text=output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
